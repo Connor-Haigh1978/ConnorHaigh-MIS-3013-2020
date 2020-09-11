0944cc03c6683f0ba23ef57421c0f976d4693b92
Connor Haigh <Connor.M.Haigh-1@ou.edu>
2020-09-10 19:30:03 -0500

    CoinFlip

---
+++ CoinToss/CoinToss/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CoinToss
+{
+    class Program
+    {
+        //Connor Haigh 113408657
+        const string DEVELOPER = "Connor Haigh";
+        const int Heads = 1;
+        const int Tails = 0;
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(0, 2);
+            Console.WriteLine("Heads or Tails?:");
+            string answer = Console.ReadLine();
+
+            if (number == 1 && answer == "Heads")
+            {
+                Console.WriteLine("It's Heads! You've Won!");
+            } 
+            else if (number == 2 && answer == "Tails")
+            {
+                Console.WriteLine("It's Tails! You've Won!");
+            }
+            else
+            {
+                Console.WriteLine("Wrong Choice Loser!");
+
+            }        
+        
+        }   
+        
+
+	}
+}
---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+            //int remainder = number % 3;
+            //bool hasRemainder = remainder == 0;
+
+            //BOOLEAN OPERATORS:
+            //      OR  = ||
+            //      AND = &&
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+
+        }
+    }
+}
