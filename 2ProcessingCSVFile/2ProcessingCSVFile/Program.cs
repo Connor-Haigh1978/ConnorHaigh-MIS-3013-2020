5055bd678dfc83686467873c9bcd7fe92bfb7dc2
Connor Haigh <Connor.M.Haigh-1@ou.edu>
2020-10-15 10:47:12 -0500

    FINAL COMMIT

--- 2ProcessingCSVFile/2ProcessingCSVFile/Program.cs
+++ 2ProcessingCSVFile/2ProcessingCSVFile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace _2ProcessingCSVFile
 {
@@ -6,7 +7,39 @@ namespace _2ProcessingCSVFile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
+            /* 
+             * Transaction_date = 0
+             * Payment = 3
+             * Name = 4
+             * Country = 7
+             * 
+             * */
+            
+            string filepath = @"C:\Users\conno\Downloads\SalesJan2009.csv";
+            string[] lines = File.ReadAllLines(filepath);
+            int q = 0;
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split(',');
+                string date = pieces[0];
+                //string payment = pieces[3];
+                string name = pieces[4];
+                //string country = pieces[7];
+                //bool condition = (payment.ToLower() == "visa" || country.ToLower() == "united states");
+
+                if (pieces[3].ToLower() == "visa" & pieces[7].ToLower() == "united states")
+                {
+                    Console.WriteLine($"{name} {date}");
+                    q += 1; // this is how I checked I got the right amount of values.
+                }
+
+            }
+
+            Console.WriteLine(q);
+            Console.ReadKey();
+           
         }
     }
 }
