ee1d4230aa8c5ce8d13b9f82c86d80a84dda9e1c
Connor Haigh <Connor.M.Haigh-1@ou.edu>
2020-10-21 10:55:20 -0500

    FINAL COMMit

---
+++ MethodsParticipation/MethodsParticipation/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace MethodsParticipation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me an animal please (cow, goat, monkey)");
+            string Animal = Console.ReadLine().ToLower();
+            string noise = AnimalNoise(Animal);
+            Console.ReadKey();
+ 
+        }
+        static string AnimalNoise(string Noise)
+        {
+            
+            if (Noise == "cow")
+            {
+                Console.WriteLine("MOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO");
+            }
+            if (Noise == "monkey")
+            {
+                Console.WriteLine("FGOUYFIYGOHUPIUFGPIGPIG");
+            }
+            if (Noise == "goat")
+            {
+                Console.WriteLine("*Lebron James Makes Noises*");
+            }
+            else
+            {
+                Console.WriteLine("Input valid animal");
+            }
+            return Noise;
+        }
+
+    }
+}
