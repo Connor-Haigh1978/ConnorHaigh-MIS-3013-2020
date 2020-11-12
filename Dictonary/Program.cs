0b43933f9a1d913e634be013a3dcd0ed4a584564
Connor Haigh <Connor.M.Haigh-1@ou.edu>
2020-11-12 14:15:45 -0600

    COMMIT

---
+++ Dictonary/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Dictonary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //WordDictionary<string, int> Counts = new Dictonary<string, int>(); 
+
+            string FilePath = @"C:\Users\conno\Downloads\Jane Eyre.txt";
+            string[] lines = File.ReadAllLines(FilePath);
+            string line;
+            string[] words;
+
+
+            int count;
+            
+            bool AnotherIteration = true;
+
+            Dictionary<string, int> ItemCount = new Dictionary<string, int>();
+            string word;
+
+            do
+            {
+                string question = "What word would you like to look for in Jane Eyre?";
+                Console.WriteLine(question);
+                word = Console.ReadLine();
+                count = 0;
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    line = lines[i];
+
+                    if (line.Contains(","))
+                    {
+                        line = line.Replace(",", " ");
+                    }
+                    if (line.Contains("."))
+                    {
+                        line = line.Replace(".", " ");
+                    }
+                    if (line.Contains(";"))
+                    {
+                        line = line.Replace(";", " ");
+                    }
+                    if (line.Contains(":"))
+                    {
+                        line = line.Replace(":", " ");
+                    }
+                    if (line.Contains("("))
+                    {
+                        line = line.Replace("(", " ");
+                    }
+                    if (line.Contains(")"))
+                    {
+                        line = line.Replace(")", " ");
+                    }
+                    if (line.Contains("?"))
+                    {
+                        line = line.Replace("?", " ");
+                    }
+                    if (line.Contains("_"))
+                    {
+                        line = line.Replace("_", " ");
+                    }
+                    words = line.Split(" ");
+
+                    foreach (var item in words)
+                    {
+                        if (item == word)
+                        {
+                            count++;
+                        }
+                    }
+
+                }
+                ItemCount.Add(word, count);
+                if (count == 0)
+                {
+                    Console.WriteLine("Word cannot be found");
+                }
+                Console.WriteLine("Would you like to look for another word? (y/n)");
+                if (Console.ReadLine().ToLower() == "y")
+                {
+                    AnotherIteration = true;
+                }
+                else
+                {
+                    AnotherIteration = false;
+                }
+
+
+            } while (AnotherIteration);
+
+            Console.WriteLine("Jane Eyre Dictionary");
+            Console.WriteLine("Word           Count");
+            foreach (var input in ItemCount)
+            {
+                Console.WriteLine("{0}               {1}", input.Key, input.Value);  //found this on this website https://www.c-sharpcorner.com/UploadFile/mahesh/how-to-read-all-items-of-a-dictionary-with-C-Sharp/
+                                                                                     // Console.WriteLine($"{ItemCount.Keys}           {ItemCount.Values}");
+            }
+
+            Console.ReadKey();
+
+
+            //string iteration = "Do you want another iteration";
+            //bool AnotherIteration = true;
+            //Dictionary<string, int> ItemCount = new Dictionary<string, int>();
+            //int count = 0;
+
+            //do
+            //{
+            //    Console.WriteLine("question");
+            //    string word = Console.ReadLine();
+            //    Console.WriteLine(iteration);
+            //    count++;
+            //    ItemCount.Add(word, count);
+            //    if (Console.ReadLine().ToLower() == "y")
+            //    {
+            //        AnotherIteration = true;
+            //    }
+            //    else
+            //    {
+            //        AnotherIteration = false;
+            //    }
+            //} while (AnotherIteration);
+            //foreach (var word in ItemCount)
+            //{
+            //    Console.WriteLine(word);
+            //}
+            //Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ P-CircleRect/P-CircleRect/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+namespace P_CircleRect
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> FirstNames = new List<string>();
+            List<string> LastNames = new List<string>();
+            List<string> dobs = new List<string>();
+            List<string> FavoriteFood = new List<string>();
+            List<double> AccountBalance = new List<double>();
+
+            FirstNames.Add("Connor");
+            LastNames.Add("Haigh");
+            dobs.Add("04/19/2000");
+            FavoriteFood.Add("Steak");
+            AccountBalance.Add(420690);
+
+            FirstNames.Add("Lil");
+            LastNames.Add("Gooch");
+            dobs.Add("04/19/1969");
+            FavoriteFood.Add("Ass");
+            AccountBalance.Add(696969);
+
+            for (int i = 0; i < FirstNames.Count; i++)
+            {
+                Console.WriteLine($"First Name: {FirstNames[i]}, Last Name: {LastNames[i]}, DOB: {dobs[i]}, Favorite Food:{FavoriteFood[i]}, Account Balance: {AccountBalance[i]} ");
+            }
+
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
